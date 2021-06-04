18b9e0cfd6a27cad7131c51849bebf558ae5580c
salbornozt <49846691+salbornozt@users.noreply.github.com>
2021-06-03 19:01:23 -0500

    Update Conexion.php

--- Persistencia/Util/Conexion.php
+++ Persistencia/Util/Conexion.php
@@ -5,7 +5,7 @@ class Conexion
    {
       $user =  "postgres";
       $password = "12345678";
-      $conexion = pg_connect("host=proyectochibchaweb.cxiitfntvisw.us-east-2.rds.amazonaws.com port=5432 dbname=proyectochibchaweb user=$user password=$password");
+      $conexion = pg_connect("host=proyectochibchaweb.cxiitfntvisw.us-east-2.rds.amazonaws.com port=5432 dbname=ProyectoChibchaWeb user=$user password=$password");
       return $conexion;
    }
    public function cerrarDB($conexion)
